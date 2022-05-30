37bb22119066260c490d356c4e9c9febebc06b99
Yonela-Johannes <johannesyonela@gmail.com>
2022-05-30 16:00:42 +0200

    Test module

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -1,6 +1,6 @@
+// create Factory Function instance
 import { TaxiQueue } from "./taxi-queue.js";
 const taxiQueue = TaxiQueue();
-
 // write your DOM code here.
 let passengerCounter = document.querySelector('.passenger_queue_count')
 let taxiQueCount = document.querySelector('.taxi_queue_count')
@@ -10,7 +10,6 @@ const joinBtn = document.querySelector('.join_queue')
 const leaveBtn = document.querySelector('.leave_queue')
 const joinTaxiBtn = document.querySelector('.join_taxi_queue')
 const departBtn = document.querySelector('.depart')
-// create Factory Function instance
 const join = () => {
     taxiQueue.joinQueue()
     passengerCounter.innerHTML = taxiQueue.queueLength()
@@ -29,7 +28,7 @@ const joinTaxiQueue = () => {
 const departTaxi = () => {
     taxiQueue.taxiDepart()
     console.log(taxiQueue.taxiDepart())
-    // departCount.innerHTML = 
+    departCount.innerHTML = taxiQueue.taxiDepart()
 }
 // DOM events
 joinBtn.addEventListener('click', join)
--- taxi-queue.js
+++ taxi-queue.js
@@ -47,5 +47,3 @@ function TaxiQueue() {
 		taxiDepart
 	}
 }
-
-export { TaxiQueue }
